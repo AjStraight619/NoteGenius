3db603d3d4caa9c6b98a1069e37a0ebd7fdc056e
Alex Straight <compsciaj@outlook.com>
2023-09-27 00:41:23 -0700

    added calculator, initializing ui for wolframalpha api integration

---
+++ app/math/page.tsx
@@ -0,0 +1,11 @@
+import Calculator from "@/components/component/calculator/Calculator";
+
+const Math = () => {
+  return (
+    <div>
+      <Calculator />
+    </div>
+  );
+};
+
+export default Math;
