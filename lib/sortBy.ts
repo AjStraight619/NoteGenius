2ccf81357071440e0069f15a84c0ad691cead0f0
AJ Straight <ajstraight@AJs-MacBook-Pro.local>
2023-09-28 02:14:31 -0700

    Updated type names in sort file

--- lib/sortBy.ts
+++ lib/sortBy.ts
@@ -1,6 +1,10 @@
 import { SortableItem } from "@/types/otherTypes";
-import type { Note, RefinedNote, Tag } from "@prisma/client";
+import type { File as PrismaFile } from "@prisma/client";
 import { Folder } from "@prisma/client";
+
+type ExtendedFile = PrismaFile & {
+  refinedFile?: any;
+};
 
 export const sortByAz = (items: SortableItem[]): SortableItem[] => {
   if (!Array.isArray(items)) {
@@ -16,52 +20,36 @@ export const sortByZa = (items: SortableItem[]): SortableItem[] => {
   return items.sort((a, b) => b.name.localeCompare(a.name));
 };
 
-export const sortByRefined = (items: Note[]): Note[] => {
+export const sortByRefined = (items: ExtendedFile[]): ExtendedFile[] => {
   return items.sort((a, b) => {
-    if (a.isRefined && !b.isRefined) {
+    if (a.refinedFile && !b.refinedFile) {
       return -1;
     }
-    if (!a.isRefined && b.isRefined) {
+    if (!a.refinedFile && b.refinedFile) {
       return 1;
     }
     return 0;
   });
 };
 
-export const sortByNotRefined = (items: Note[]): Note[] => {
+export const sortByNotRefined = (items: ExtendedFile[]): ExtendedFile[] => {
   return items.sort((a, b) => {
-    if (a.isRefined && !b.isRefined) {
+    if (a.refinedFile && !b.refinedFile) {
       return 1;
     }
-    if (!a.isRefined && b.isRefined) {
+    if (!a.refinedFile && b.refinedFile) {
       return -1;
     }
     return 0;
   });
 };
 
-// export const sortByTags = (folders: Folder[], tags: string[]): Folder[] => {
-//   return folders.sort((a, b) => {
-//     for (const tag of tags) {
-//       const aHasTag = a.tags.includes(tag);
-//       const bHasTag = b.tags.includes(tag);
-
-//       if (aHasTag && !bHasTag) return -1;
-//       if (!aHasTag && bHasTag) return 1;
-//     }
-
-//     return 0;
-//   });
-// };
-
-// export const sortByData = (folders: Folder[], data:
-
-export const excludeRefined = (items: Note[]): Note[] => {
-  return items.filter((item) => !item.isRefined);
+export const excludeRefined = (items: ExtendedFile[]): ExtendedFile[] => {
+  return items.filter((item) => !item.refinedFile);
 };
 
-export const includeOnlyRefined = (items: Note[]): Note[] => {
-  return items.filter((item) => item.isRefined);
+export const includeOnlyRefined = (items: ExtendedFile[]): ExtendedFile[] => {
+  return items.filter((item) => item.refinedFile);
 };
 
 export const sortByFirstCreated = (items: SortableItem[]): SortableItem[] => {
