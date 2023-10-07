deb5d8c6f63033c3832f06c2f483d16262197309
Alex Straight <compsciaj@outlook.com>
2023-10-06 17:06:42 -0700

    Changed response to just display text from gpt

--- app/google/page.tsx
+++ app/google/page.tsx
@@ -1,12 +1,64 @@
+// "use client";
+// import React, { useState } from "react";
+
+// type TextAnnotation = {
+//   description: string;
+// };
+
+// export default function ImageUpload() {
+//   const [detections, setDetections] = useState<TextAnnotation[] | null>(null);
+
+//   const handleImageChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+//     const files = e.target.files;
+//     if (files && files.length > 0) {
+//       const image = files[0];
+//       console.log("Uploading image:", image.name);
+
+//       const formData = new FormData();
+//       formData.append("image", image);
+
+//       try {
+//         const response = await fetch("/api/google", {
+//           method: "POST",
+//           body: formData,
+//         });
+
+//         const data = await response.json();
+
+//         setDetections(data);
+//       } catch (error) {
+//         console.error("Error uploading image:", error);
+//       }
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <input
+//         type="file"
+//         accept="image/*,.heic"
+//         capture="environment"
+//         onChange={handleImageChange}
+//       />
+//       {detections && (
+//         <div className="flex items-center">
+//           <h3>Detected Text:</h3>
+//           <ul>
+//             {detections.map((text, index) => (
+//               <li key={index}>{text.description}</li>
+//             ))}
+//           </ul>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
 "use client";
 import React, { useState } from "react";
 
-type TextAnnotation = {
-  description: string;
-};
-
 export default function ImageUpload() {
-  const [detections, setDetections] = useState<TextAnnotation[] | null>(null);
+  const [refinedText, setRefinedText] = useState<string | null>(null);
 
   const handleImageChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
@@ -23,9 +75,9 @@ export default function ImageUpload() {
           body: formData,
         });
 
-        const data = await response.json();
+        const data = await response.text(); // assuming the response is plain text
 
-        setDetections(data);
+        setRefinedText(data);
       } catch (error) {
         console.error("Error uploading image:", error);
       }
@@ -40,14 +92,10 @@ export default function ImageUpload() {
         capture="environment"
         onChange={handleImageChange}
       />
-      {detections && (
+      {refinedText && (
         <div className="flex items-center">
-          <h3>Detected Text:</h3>
-          <ul>
-            {detections.map((text, index) => (
-              <li key={index}>{text.description}</li>
-            ))}
-          </ul>
+          <h3>Refined Text:</h3>
+          <p>{refinedText}</p>
         </div>
       )}
     </div>
